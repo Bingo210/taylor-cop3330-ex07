e16c402d01701f9fb9f083aa43429db82ea3ec9c
Noah Taylor <noahtaylor243@knights.ucf.edu>
2021-09-11 21:17:17 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 5 Solution
+ * Copyright 2021 Noah Taylor
+ */
+
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static double MetricDimensions(double squareFeet){
+
+        return squareFeet * 0.09290304;
+    }
+
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println( "What is the length of the room in feet?" );
+        double feetLength = input.nextDouble();
+
+        System.out.println( "What is the width of the room in feet?" );
+        double feetWidth = input.nextDouble();
+
+        System.out.println( "You entered dimensions of " + feetLength +
+                " feet by " + feetWidth + " feet.");
+
+        double squareFeet = feetLength * feetWidth;
+        double squareMeters = MetricDimensions(squareFeet);
+
+        System.out.println("The area is" + "\n" + squareFeet + " square feet " +
+                "\n" + squareMeters + " square meters");
+    }
+}
